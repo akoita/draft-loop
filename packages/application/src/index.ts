e889f74fb55da2de7cf65e57eb7a4bf04df039a5
akoita <aboubakar.koita@yahoo.fr>
2026-09-19 17:24:44 +0200

    test(application): add rejected-author replay

--- packages/application/src/index.ts
+++ packages/application/src/index.ts
@@ -569,4 +569,5 @@ export * from "./opportunity-brief.js";
 export * from "./opportunity-extraction.js";
 export * from "./opportunity-intake.js";
 export * from "./opportunity-persistence.js";
+export * from "./rejected-author-replay.js";
 export * from "./requirement-achievement-plan.js";
